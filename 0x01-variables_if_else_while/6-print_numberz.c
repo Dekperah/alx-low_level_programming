28d2fc481e620e87d67c4724e3a4711351dcbfab
David Ekperah <dekperah@gmail.com>
2022-09-10 00:22:06 -0700

    Numberz completed

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,12 +9,11 @@
 
 int main(void)
 {
-	int n = 48;
-	while (n <= 57)
-	{
-		putchar(n);
-		n += 1;
-	}
-	putchar("\n");
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+
 	return (0);
 }
