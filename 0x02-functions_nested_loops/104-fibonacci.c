34b3f229c9cfa7162fcf73419035a387a7a7fdd8
Dekperah <111078021+Dekperah@users.noreply.github.com>
2022-09-14 04:56:30 +0100

    104-fibonacci fixed

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,26 +8,41 @@
 
 int main(void)
 {
-	int counter = 2;
+	int count;
+	unsigned long i, j, k;
+	unsigned long m, n, p, carry;
 
-	float num1 = 1;
-	float num2 = num1 + 1;
-	float num3 = num1 + num2;
-
-	printf("%.0f, ", num1);
-	printf("%.0f, ", num2);
-	while (counter < 98)
+	count = 0;
+	i = 0;
+	j = 1;
+	for (count = 1; count <= 91; count++)
 	{
-		counter++;
-		printf("%.0f", num3);
-		num1 = num2;
-		num2 = num3;
-		num3 = num1 + num2;
-		if (counter < 98)
-		{
-			printf(", ");
-		}
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%lu, ", k);
 	}
-	printf("\n");
+	m = i % 1000;
+	i = i / 1000;
+	n = j % 1000;
+	j = j / 1000;
+	while (count <= 98)
+	{
+		carry = (m + n) / 1000;
+		p = (m + n) - carry * 1000;
+		k = (i + j) + carry;
+		m = n;
+		n = p;
+		i = j;
+		j = k;
+		if (p >= 100)
+			printf("%lu%lu", k, p);
+		else
+			printf("%lu0%lu", k, p);
+		if (count != 98)
+			printf(", ");
+		count++;
+	}
+	putchar('\n');
 	return (0);
 }
