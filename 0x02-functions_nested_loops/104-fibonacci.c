161c0fd6a84663125d5ca89d3ca26a4a60a9e88d
David Ekperah <dekperah@gmail.com>
2022-09-13 21:17:10 -0700

    104-fibonacci completed

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,36 +9,36 @@
 int main(void)
 {
 	int count;
-	unsigned long i, j, k;
-	unsigned long m, n, p, carry;
+	unsigned long num1, num2, num3;
+	unsigned long x1, x2, x3, carry;
 
 	count = 0;
-	i = 0;
-	j = 1;
+	num1 = 0;
+	num2 = 1;
 	for (count = 1; count <= 91; count++)
 	{
-		k = i + j;
-		i = j;
-		j = k;
-		printf("%lu, ", k);
+		num3 = num1 + num2;
+		num1 = num2;
+		num2 = num3;
+		printf("%lu, ", num3);
 	}
-	m = i % 1000;
-	i = i / 1000;
-	n = j % 1000;
-	j = j / 1000;
+	x1 = num1 % 1000;
+	num1 = num1 / 1000;
+	x2 = num2 % 1000;
+	num2 = num2 / 1000;
 	while (count <= 98)
 	{
-		carry = (m + n) / 1000;
-		p = (m + n) - carry * 1000;
-		k = (i + j) + carry;
-		m = n;
-		n = p;
-		i = j;
-		j = k;
+		carry = (x1 + x2) / 1000;
+		x3 = (x1 + x2) - carry * 1000;
+		num3 = (num1 + num2) + carry;
+		x1 = x2;
+		x2 = x3;
+		num1 = num2;
+		num2 = num3;
 		if (p >= 100)
-			printf("%lu%lu", k, p);
+			printf("%lu%lu", num3, x3);
 		else
-			printf("%lu0%lu", k, p);
+			printf("%lu0%lu", num3, x3);
 		if (count != 98)
 			printf(", ");
 		count++;
