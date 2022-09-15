0d801c805ed844c1938b5f029e6aab3618093f8d
David Ekperah <dekperah@gmail.com>
2022-09-15 15:47:10 -0700

    I believe in numbers and signs completed

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
   * print_most_numbers - Print the numbers since 0 up to 9
@@ -9,17 +9,17 @@
   */
 void print_most_numbers(void)
 {
-	int x = 0;
+	int num = 0;
 
-	for (; x <= 9; x++)
+	for (; num <= 9; num++)
 	{
-		if (x == 2 || x == 4)
+		if (num == 2 || num == 4)
 		{
 			continue;
 		}
 		else
 		{
-			_putchar(x + '0');
+			_putchar(num + '0');
 		}
 	}
 
