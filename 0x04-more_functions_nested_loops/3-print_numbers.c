af44a2ff1102ae4e0c23e9c5d83af32ff63876eb
David Ekperah <dekperah@gmail.com>
2022-09-15 15:40:10 -0700

    The numbers speak for themselves completed

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,16 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-  * print_numbers - Print the numbers since 0 up to 9
-  *
-  * Return: The numbers since 0 up to 9
-  */
+ * print_numbers - Print the numbers since 0 up to 9
+ * Return: The numbers since 0 up to 9
+ */
 void print_numbers(void)
 {
-	int x;
+	int num;
 
-	for (x = 0; x <= 9; x++)
+	for (num = 0; num <= 9; num++)
 	{
-		_putchar(x + '0');
+		_putchar(num + '0');
 	}
 
 	_putchar('\n');
