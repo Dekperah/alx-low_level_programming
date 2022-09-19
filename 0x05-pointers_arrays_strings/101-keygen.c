2eb07fde84803d9909e48403f35a0f1521b89524
David Ekperah <dekperah@gmail.com>
2022-09-19 11:50:45 -0700

    Don't hate the hacker, hate the code task completed

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,16 +1,9 @@
-/*
- * File: 101-keygen.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Generates random valid passwords for the
  *        program 101-crackme.
- *
  * Return: Always 0.
  */
 int main(void)
@@ -19,7 +12,6 @@ int main(void)
 	int index = 0, sum = 0, diff_half1, diff_half2;
 
 	srand(time(0));
-
 	while (sum < 2772)
 	{
 		password[index] = 33 + rand() % 94;
@@ -27,14 +19,12 @@ int main(void)
 	}
 
 	password[index] = '\0';
-
 	if (sum != 2772)
 	{
 		diff_half1 = (sum - 2772) / 2;
 		diff_half2 = (sum - 2772) / 2;
 		if ((sum - 2772) % 2 != 0)
 			diff_half1++;
-
 		for (index = 0; password[index]; index++)
 		{
 			if (password[index] >= (33 + diff_half1))
@@ -52,8 +42,6 @@ int main(void)
 			}
 		}
 	}
-
 	printf("%s", password);
-
 	return (0);
 }
