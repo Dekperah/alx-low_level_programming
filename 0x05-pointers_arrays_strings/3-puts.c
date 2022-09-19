e4245fd8589675b77a0e220668eac5b68ab6abb1
David Ekperah <dekperah@gmail.com>
2022-09-19 01:34:55 -0700

    I do not fear computers. I fear the lack of them task completed

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,10 @@
-/*
- * File: 3-puts.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _puts - Prints a string to stdout.
  * @str: The string to be printed.
  */
+
 void _puts(char *str)
 {
 	while (*str)
