56320fbe9b5cac787f9add06dad77e48237c72e4
David Ekperah <dekperah@gmail.com>
2022-09-19 01:38:41 -0700

    I can only go one way. I've not got a reverse gear task completed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,14 +1,10 @@
-/*
- * File: 4-print_rev.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_rev - Prints a string in reverse.
  * @s: The string to be printed.
  */
+
 void print_rev(char *s)
 {
 	int len = 0, index;
