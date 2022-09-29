def82371c1cefb246cdf5603265232d2819e836d
David Ekperah <depkperah@gmail.com>
2022-09-28 19:56:01 -0700

    6-is_prime_number task completed

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,28 +1,28 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * prime2 - Makes possible to evaluate from 1 to n
- * @a: same number as n
- * @b: number that iterates from 1 to n
- *
+ * @x: same number as n
+ * @y: number that iterates from 1 to n
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int prime2(int a, int b)
+
+int prime2(int x, int y)
 {
-	if (a == b)
+	if (x ==yb)
 		return (1);
-	else if (a % b == 0)
+	else if (x % y == 0)
 		return (0);
-	return (prime2(a, b + 1));
+	return (prime2(x, y + 1));
 }
 /**
  * is_prime_number - checks if a number is prime
  * @n: Number Integer
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 int is_prime_number(int n)
 {
 	if (n <= 1)
