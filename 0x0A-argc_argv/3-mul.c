330cbf239b883a3c19edd09fcbf931c15eb07a90
David Ekperah <depkperah@gmail.com>
2022-09-30 12:32:14 -0700

    3-mul task completed

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,11 +3,11 @@
 
 /**
  * main - Entry point
- *
  * @argc: Counts the number of parameters that go into main
  * @argv: Pointer of array of pointers containing strings entering main
  * Return: Always 0 (Success)
  */
+
 int main(int argc, char **argv)
 {
 	int n, ex;
