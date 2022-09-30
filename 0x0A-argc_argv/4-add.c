dd43e0e699a1c8cc1a46e3bf308ea68a455e428d
David Ekperah <depkperah@gmail.com>
2022-09-30 12:36:33 -0700

    4-add task completed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,25 +2,24 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-
 /**
  * _isnumber - checks if string is a number
  * @s: string
- *
  * Return: On success 1.
  * If not a number, 0 is returned.
  */
+
 int _isnumber(char *s)
 {
-	int i, check, d;
+	int a, check, z;
 
-	i = 0, d = 0, check = 1;
+	a = 0, z = 0, check = 1;
 	if (*s == '-')
-		i++;
-	for (; *(s + i) != 0; i++)
+		a++;
+	for (; *(s + a) != 0; a++)
 	{
-		d = isdigit(*(s + i));
-		if (d == 0)
+		z = isdigit(*(s + a));
+		if (z == 0)
 		{
 			check = 0;
 			break;
