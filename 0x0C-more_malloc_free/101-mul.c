fd52ee2f5ecc740fce510ca446c59be1b6425957
David Ekperah <depkperah@gmail.com>
2022-10-06 14:21:40 -0700

    101-mul task completed

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+
 /**
  * _isnumber - checks if string is number
- *
  * @s: string
- *
  * Return: 1 if number, 0 if not
  */
+
 int _isnumber(char *s)
 {
 	int i, check, d;
