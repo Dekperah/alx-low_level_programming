f3d6faf0defda49ddab7af5daea35b59f978a58e
David Ekperah <depkperah@gmail.com>
2022-10-10 18:52:44 -0700

    1-init_dog task completed

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+
 /**
  * init_dog - writes the character c to stdout
  * @d: Dog
@@ -10,6 +11,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != 0)
