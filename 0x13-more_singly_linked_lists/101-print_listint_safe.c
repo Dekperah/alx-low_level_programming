54779811da27799e41bc8a9d2ccd6a398f55c547
David Ekperah <depkperah@gmail.com>
2022-10-24 21:30:47 -0700

    101-print_listint_safe task

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,9 +5,9 @@
 /**
  * print_listint_safe - prints a singly linked list (can print lists with loop)
  * @head: head pointer of singly linked list
- *
  * Return: Number of elements in list
  */
+
 size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *tmp, *tmp2;
