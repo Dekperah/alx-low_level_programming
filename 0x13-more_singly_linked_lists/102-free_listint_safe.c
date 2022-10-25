9847fd292390315e3ca0f1134f2fca93fd510d9f
David Ekperah <depkperah@gmail.com>
2022-10-24 21:26:55 -0700

    102-free_listint_safe task

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -5,9 +5,9 @@
 /**
  * free_listint_safe - frees a listint_t list (can free lists with a loop)
  * @h: pointer to head of singly linked list
- *
  * Return: the size of the list that was free’d
  */
+
 size_t free_listint_safe(listint_t **h)
 {
 	listint_t *tmp = *h, *tmp2;
