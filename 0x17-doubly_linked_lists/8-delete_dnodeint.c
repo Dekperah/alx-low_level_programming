dc7fbecc82037d835202d5ce1562a76c51be57ff
David Ekperah <depkperah@gmail.com>
2022-12-08 20:43:34 -0800

    8-delete_dnodeint

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,6 +9,7 @@
  *
  * Return: 1 if it succeeded, -1 if it failed
  */
+
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *tmpcount = *head, *tmp1 = *head, *tmp2, *tmp3;
